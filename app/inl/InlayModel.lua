36d338c2181ac09725c9da155b7b3fb0479fc752
DonnieSuen <651991167@qq.com>
2014-11-17 18:49:01 +0800

    no message

--- app/inl/InlayModel.lua
+++ app/inl/InlayModel.lua
@@ -1,3 +1,7 @@
+--
+-- Author: Fangzhongzheng
+-- Date: 2014-10-30 15:31:35
+--
 import("..includes.functionUtils")
 
 local InlayModel = class("InlayModel", cc.mvc.ModelBase)
@@ -7,15 +11,15 @@ function InlayModel:ctor(properties, events, callbacks)
 	self:addComponent("components.behavior.EventProtocol"):exportMethods()
 end
 
-function InlayModel:getConfigTable(propertyName, index)
-	assert(propertyName and index, "invalid param")
+function InlayModel:getConfigTable(fileName, index)
+	assert(fileName and index, "invalid param")
 	local config = getConfig("config/items_xq.json")
-	local records = getRecord(config, propertyName, index) or {}
+	local records = getRecord(config, fileName, index) or {}
 	return records
 end
 
-function InlayModel:refreshBtnIcon(string,index)
-	self:dispatchEvent({name = "REFRESH_BTN_ICON_EVENT", string = string,index = index})
+function InlayModel:refreshBtnIcon(string, index)
+	self:dispatchEvent({name = "REFRESH_BTN_ICON_EVENT", string = string, index = index})
 end
 
 return InlayModel
--- app/inlay/InlayListCell.lua
+++ app/inlay/InlayListCell.lua
@@ -1,95 +1,42 @@
---
--- Author: Fangzhongzheng
--- Date: 2014-10-31 12:54:04
---
-import("..includes.functionUtils")
+local ScrollViewCell = import("..includes.ScrollViewCell")
 local InlayModel = import(".InlayModel")
-local InlayListCell = class("InlayListCell", function()
-    return display.newLayer()
-end)
 
-function InlayListCell:ctor()
-    -- Setting swallow, otherwise, the rootNode will cover the left buttons.
-    self:setTouchEnabled(true)
-    self:setTouchSwallowEnabled(false)
+local InlayListCell = class("InlayListCell", ScrollViewCell)
+
+function InlayListCell:ctor(record)
     self.inlayModel = app:getInstance(InlayModel)
+	self:initCellUI(record)
 end
 
-function InlayListCell:getListCell(string, index)
-    cc.FileUtils:getInstance():addSearchPath("res/Inlay/")
-    local table = self.inlayModel:getConfigTable("type", string)
+function InlayListCell:initCellUI(record)
+	cc.FileUtils:getInstance():addSearchPath("res/InlayShop/")
+	local controlNode = cc.uiloader:load("xiangqian_type.json")
+    local imgPanel = cc.uiloader:seekNodeByName(controlNode, "imgPanel")
+    local Img = cc.ui.UIImage.new(record["imgnam"]..".png")
+    addChildCenter(Img, imgPanel)
 
-    -- Create the kind of "type = 1" listView cells
-    local content
-    if string == "demage" or string == "secure" or
-     string == "clip" or string == "bullet" then
-        -- loac ccs
-        content = cc.uiloader:load("xiangqian_type1.ExportJson")
-        local title = cc.uiloader:seekNodeByName(content, "label_title")
-        local imgPanel = cc.uiloader:seekNodeByName(content, "Panel_img")
-        local describe = cc.uiloader:seekNodeByName(content, "Label_describe")
-        title:setString((table[index])["name"])
-        describe:setString((table[index])["describe"])
-        local img=cc.ui.UIImage.new((table[index])["imgName"]..".png")
-        addChildCenter(img, imgPanel)
+    local btnBuy = cc.uiloader:seekNodeByName(controlNode, "buyBtn")
+    local btnLoad = cc.uiloader:seekNodeByName(controlNode, "loadBtn")
+    addBtnEventListener(btnBuy, function(event)
+            if event.name=='began' then
+                print("btnBuy is begining!")
+                return true
+            elseif event.name=='ended' then
+                print("btnBuy is pressed!")
+            end
+        end)
+    addBtnEventListener(btnLoad, function(event)
+            if event.name=='began' then
+                print("btnLoad is begining!")
+                return true
+            elseif event.name=='ended' then
+                print("btnLoad is pressed!")
+                self.inlayModel:refreshBtnIcon(record["type"],record["id"])
+            end
+        end)
 
-        -- 获得两个按钮并设置监听
-        local btnBuy = cc.uiloader:seekNodeByName(content, "btn_buy")
-        local btnLoad = cc.uiloader:seekNodeByName(content, "btn_load")
-        addBtnEventListener(btnBuy, function(event)
-                if event.name=='began' then
-                    print("btnBuy is begining!")
-                    return true
-                elseif event.name=='ended' then
-                    print("btnBuy is pressed!")
-                end
-            end)
-        addBtnEventListener(btnLoad, function(event)
-                if event.name=='began' then
-                    print("btnLoad is begining!")
-                    return true
-                elseif event.name=='ended' then
-                    print("btnLoad is pressed!")
-                    self.inlayModel:refreshBtnIcon(string, index)
-                end
-            end)
-
-    -- Create the kind of "type = 2" listView cells
-    else
-        -- load ccs
-        content = cc.uiloader:load("xiangqian_type2.ExportJson")
-        local title = cc.uiloader:seekNodeByName(content, "label_title")
-        local imgPanel = cc.uiloader:seekNodeByName(content, "Panel_img")
-        local describe = cc.uiloader:seekNodeByName(content, "describe")
-        local ownNum = cc.uiloader:seekNodeByName(content, "label_ownNum")
-        title:setString((table[index])["name"])
-        describe:setString((table[index])["describe"])
-        ownNum:setString("0")
-        local img=cc.ui.UIImage.new((table[index])["imgName"]..".png")
-        addChildCenter(img, imgPanel)
-
-        -- 获得两个按钮并设置监听
-        local btnBuy = cc.uiloader:seekNodeByName(content, "btn_buy")
-        local btnLoad = cc.uiloader:seekNodeByName(content, "btn_load")
-        addBtnEventListener(btnBuy, function(event)
-                if event.name=='began' then
-                    print("btnBuy is begining!")
-                    return true
-                elseif event.name=='ended' then
-                    print("btnBuy is pressed!")
-                end
-            end)
-        addBtnEventListener(btnLoad, function(event)
-                if event.name=='began' then
-                    print("btnLoad is begining!")
-                    return true
-                elseif event.name=='ended' then
-                    print("btnLoad is pressed!")
-                    self.inlayModel:refreshBtnIcon(string, index)
-                end
-            end)
-    end
-    return content
+	controlNode:setPosition(0, 0)
+    self:addChild(controlNode)
 end
 
 return InlayListCell
