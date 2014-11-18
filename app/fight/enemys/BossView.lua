39d4790421a88d42a79d956f4c3e628efedb7ca2
yanxin59 <359063370@qq.com>
2014-11-18 15:48:31 +0800

    no message

--- app/fight/enemys/BossView.lua
+++ app/fight/enemys/BossView.lua
@@ -45,7 +45,7 @@ end
 function BossView:initBlood()
     --add blood
     cc.FileUtils:getInstance():addSearchPath("res/Fight/fightLayer/ui")
-    local node = cc.uiloader:load("UI.json")    
+    local node = cc.uiloader:load("heroUI.ExportJson")    
     self.blood = cc.uiloader:seekNodeByName(node, "enemyBlood")
     self.blood:removeFromParent()
     local bound = self.armature:getBoundingBox()
