722e20017f109717e31a32d17c8c0dd17066be3b
yby415098820 <415098820@qq.com>
2015-01-05 20:12:02 +0800

    no message

--- app/Define.lua
+++ app/Define.lua
@@ -12,7 +12,7 @@ Define.kBaoDemageOtherEnemys 	= 10  --自爆兵伤害值
 
 --机甲
 Define.kRobotCoolDownTime 		= 0.1   --robot 冷却时间
-Define.kRobotDemage			 	= 30    --机甲伤害
+Define.kRobotDemage			 	= 220    --机甲伤害
 Define.kRobotTime			 	= 10.0   --机甲持续时间
 
 --手雷
