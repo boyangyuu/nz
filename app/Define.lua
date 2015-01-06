34a1ce36e5c239c6b47c60950ad1d629df74e79f
yby415098820 <415098820@qq.com>
2015-01-06 16:30:42 +0800

    no message

--- app/Define.lua
+++ app/Define.lua
@@ -5,13 +5,13 @@ local Define = class("Define", cc.mvc.ModelBase)
 --fight
 
 --黄金武器
-Define.kGoldActivate 			= 20    --黄金武器激活
+Define.kGoldActivate 			= 15    --黄金武器激活
 
 --自爆兵
 Define.kBaoDemageOtherEnemys 	= 10  --自爆兵伤害值
 
 --机甲
-Define.kRobotCoolDownTime 		= 0.1   --robot 冷却时间
+Define.kRobotCoolDownTime 		= 0.08   --robot 冷却时间
 Define.kRobotDemage			 	= 220    --机甲伤害
 Define.kRobotTime			 	= 10.0   --机甲持续时间
 
