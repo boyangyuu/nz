253a683fb657d2517889d86d84fff6a0334d733b
firefly1991 <pengfei99999@qq.com>
2015-01-29 23:06:50 +0800

    ｌｓ

--- app/fight/fightConfigs/wave2_3.lua
+++ app/fight/fightConfigs/wave2_3.lua
@@ -240,7 +240,7 @@ local waves = {
 		},
 	},
 
-		
+
 	{
 		enemys = {
 			
@@ -254,7 +254,7 @@ local waves = {
 				descId = "boss02", --简介
 				time = 3,	
 				num = 1,
-				pos = {250},
+				pos = {500},
 				delay = {4},
 				property = { 
 					type = "boss02",
@@ -336,40 +336,79 @@ local enemys = {
 	weak1=3},                                                               --scale = 3.0,  近战走到屏幕最近放缩比例
 
 
+
 }
 
+--enemy的关卡配置
+local enemys = {
+	--普通兵
+	{id=1,image="anim_enemy_002",demage=5,hp=260,walkRate=180,walkCd=2,rollRate=180,rollCd=2,fireRate=240,fireCd=3,
+	weak1=3},
 
+	--近战兵
+	{id=2,image="jinzhanb",demage=4,hp=1080,walkRate=400,rollRate=100,fireRate=100,
+		weak1=2,weak2=2},
 
---boss的关卡配置
+	--伞兵
+	{id=3,image="sanbing01",demage=0,hp=260,
+	weak1=3},	             
+
+    --导弹          --missileType = "daodan",
+	{id=4,image="daodan",demage=10,hp=1,weak1=1},
+
+	--铁球
+	{id=5,image="tieqiu",demage=20,hp=10000,weak1=1},	
+
+	--手雷兵
+	{id=6,image="shouleib",demage=0,hp=195,walkRate=180,walkCd=2,rollRate=180,rollCd=2,fireRate=300,fireCd=4,
+	weak1=3},
+	--手雷
+	{id=7,image="shoulei",demage=10,hp=1,
+	weak1=3},
+
+	--BOSS导弹          --missileType = "daodan",
+	{id=8,image="daodan",demage=3,hp=120,weak1=1},
+
+	--忍者兵
+	{id=9,image="renzb",demage=50,hp=900,fireRate=200, fireCd=2.0,walkRate=100,walkCd = 1.0,rollRate=40, rollCd = 1.5, 
+		shanRate = 100, shanCd = 4, chongRate = 100, chongCd = 4, weak1=3,award = 60},	
+
+	--飞镖
+	{id=10,image="feibiao",demage=10,hp=300},		
+}
+
+	--boss的关卡配置
 local bosses = {
 	--第一个出场的boss
 	{
-		image = "boss02", --图片名字
+
+		image = "renzb", --图片名字
 		hp = 100000,
-		demage = 3, 			--这个是没用的 需要告诉俊松
 		fireRate = 30,               --普攻频率
 		fireCd = 4,                     --普攻cd
-
+		demage = 0,  				 --
 		walkRate = 180,                    --移动频率
 		walkCd = 2,                         --移动cd
+		rollRate = 100,					--快速移动
+		rollCd = 2,						--快速移动cd
+		shanRate = 180, 				--瞬移
+		shanCd	= 2,					
+
 		chongfengDemage = 40,                --冲锋造成伤害
 		weak1 = 1.2,						--头 弱点伤害倍数
-		weak2 = 1.2,					--手 弱点伤害倍数
-
+		weak2 = 1.2,					--左腿 弱点伤害倍数
+		weak3 = 1.2,					--右腿 弱点伤害倍数	
 		
 		skilltrigger = {   			          --技能触发(可以同时)
-			moveLeftFire = {
-				0.90,  0.50, 0.10,
-			},
-			moveRightFire = {
-				0.70,  0.30, 
-			},
+			zhaohuan = {
+				0.95,0.80,0.75,0.55,0.25
+			},               --召唤
 			chongfeng = {
-			     0.95, 0.93, 0.87, 0.84, 0.75, 0.65, 0.45,0.35, 0.25, 0.15, 0.05,
+				0.99, 0.97, 0.87,0.84, 0.75, 0.65, 0.45,0.35, 0.25, 0.15, 0.05,
 			},
-			tieqiu = {
-				0.999,0.97, 0.80, 0.60, 0.40, 0.20, 0.05,
-			},
+			weak3 = {                               --手 技能触发(可以同时)
+				0.82,0.62,0.42, 0.22,                        
+			},	
 			weak2 = {                               --手 技能触发(可以同时)
 				0.80,0.60,0.40, 0.20,                        
 			},
@@ -384,40 +423,64 @@ local bosses = {
 			},	
 			demage400 = {  
 				0.50,
-			},						
+			},	
+						
 		},
-
-
-
-		getMoveLeftAction = function ()
-			local move1 = cc.MoveBy:create(10/60, cc.p(0, 0))
-			local move2 = cc.MoveBy:create(15/60, cc.p(-18, 0))
-			local move3 = cc.MoveBy:create(13/60, cc.p(-45, 0))	
-			local move4 = cc.MoveBy:create(7/60, cc.p(-12, 0))
-			local move5 = cc.MoveBy:create(15/60, cc.p(-4, 0))
-			return cc.Sequence:create(move1, move2, move3, move4, move5)
-		end,
-
-		getMoveRightAction = function ()
-			local move1 = cc.MoveBy:create(10/60, cc.p(10, 0))
-			local move2 = cc.MoveBy:create(15/60, cc.p(30, 0))
-			local move3 = cc.MoveBy:create(10/60, cc.p(10, 0))	
-			local move4 = cc.MoveBy:create(15/60, cc.p(12, 0))
-			local move5 = cc.MoveBy:create(10/60, cc.p(4, 0))
-			return cc.Sequence:create(move1, move2, move3, move4, move5)
-		end,
+		enemys1 = {                                                   --第一波召唤的自爆兵
+			{
+				time = 2,	
+				num = 4,
+				pos = {560,660,760,460},
+				delay = {0.2,0.4,0.5,0.7},
+				property = {
+					placeName = "place3" ,
+					id = 9,
+					type = "renzhe",
+					missileId = 10,
+				},
+			},	
+		},	
+		enemys2 = {                                                   --第一波召唤的自爆兵
+			{
+				time = 2,	
+				num = 4,
+				pos = {560,660,760,460},
+				delay = {0.2,0.4,0.5,0.7},
+				property = {
+					placeName = "place4" ,
+					id = 9,
+					type = "renzhe",
+					missileId = 10,
+				},
+			},	
+		},	
+		enemys3 = {                                                   --第一波召唤的自爆兵
+			{
+				time = 2,	
+				num = 4,
+				pos = {560,660,760,460},
+				delay = {0.2,0.4,0.5,0.7},
+				property = {
+					placeName = "place5" ,
+					id = 9,
+					type = "renzhe",
+					missileId = 10,
+				},
+			},	
+		},							
 	},
 }
 
-local mapId = "map_1_6"
-
+local mapId = "map_1_2"
 local limit = 10   				--此关敌人上限
-
 
 function waveClass:ctor()
 	self.waves  = waves
 	self.enemys = enemys
 	self.bosses = bosses
 	self.mapId  = mapId
+	self.limit  = limit
+	self.goldLimits = {35}   --黄武激活所需杀人个数
 end
+
 return waveClass
