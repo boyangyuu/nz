fc2f27e9bddb3b00b98577c0d141e67292d458c8
firefly1991 <pengfei99999@qq.com>
2015-02-08 12:25:54 +0800

    修改类名包名

--- app/commonPopup/commonPopup.lua
+++ app/commonPopup/commonPopup.lua
@@ -137,7 +137,7 @@ function commonPopup:initUI(properties)
 			elseif event.name == "ended" then
 				
 				if device.platform == "android" then
-					local className = "com/hgtt/ccn/IAPControl"
+					local className = "com/hgtt/qmtx/IAPControl"
 					luaj.callStaticMethod(className, "callPhone")
 					um:event("kefu")
 					print("btncall is pressed!")
