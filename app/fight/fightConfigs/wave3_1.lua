a4e863ced3944adef8426940c78f558af0f0beee
firefly1991 <pengfei99999@qq.com>
2015-02-08 20:23:25 +0800

    no message

--- app/fight/fightConfigs/wave3_1.lua
+++ app/fight/fightConfigs/wave3_1.lua
@@ -325,9 +325,9 @@ local waves = {
 			},
 			{
 				time = 4,
-				num = 2,
-				pos = {450,800},
-				delay = {0.5,2.0},                         -- 飞机
+				num = 1,
+				pos = {450},
+				delay = {0.5},                         -- 飞机
 				property = {
 					type = "feiji" ,
 					id = 11,
@@ -338,7 +338,23 @@ local waves = {
 					startState = "enterleft",
 					lastTime = 50.0,		                                    --持续时间			
 				},
-			},		
+			},
+			{
+				time = 7,
+				num = 1,
+				pos = {800},
+				delay = {0.5},                         -- 飞机
+				property = {
+					type = "feiji" ,
+					id = 11,
+					placeName = "place10",
+					missileId = 6,
+					missileType = "daodan",
+					missileOffsets = {cc.p(250,-250), cc.p(-150, -150)},	--炮筒位置发出xy轴偏移值,第一个位置右一,第二位置个右二
+					startState = "enterleft",
+					lastTime = 50.0,		                                    --持续时间			
+				},
+			},				
 			{
 				time = 6,
 				num = 1,
@@ -508,7 +524,7 @@ local waves = {
 					missileType = "daodan",
 					missileOffsets = {cc.p(250,-250), cc.p(-150, -150)},	--炮筒位置发出xy轴偏移值,第一个位置右一,第二位置个右二
 					startState = "enterleft",
-					lastTime = 50.0,		--持续时间
+					lastTime = 40.0,		--持续时间
 					--demageScale = 2                    --伤害翻1.5倍		
 
 				},
@@ -516,9 +532,9 @@ local waves = {
 
 			{
 				time = 4,
-				num = 2,
-				pos = {350,700},
-				delay = {0.2,3.0},                         -- 飞机
+				num = 1,
+				pos = {450},
+				delay = {0.2},                         -- 飞机
 				property = {
 					type = "feiji" ,
 					id = 11,
@@ -527,9 +543,25 @@ local waves = {
 					missileType = "daodan",
 					missileOffsets = {cc.p(250,-250), cc.p(-150, -150)},	--炮筒位置发出xy轴偏移值,第一个位置右一,第二位置个右二
 					startState = "enterleft",
-					lastTime = 50.0,		                                    --持续时间			
+					lastTime = 40.0,		                                    --持续时间			
 				},
 			},	
+			{
+				time = 7,
+				num = 1,
+				pos = {850},
+				delay = {0.5},                         -- 飞机
+				property = {
+					type = "feiji" ,
+					id = 11,
+					placeName = "place10",
+					missileId = 6,
+					missileType = "daodan",
+					missileOffsets = {cc.p(250,-250), cc.p(-150, -150)},	--炮筒位置发出xy轴偏移值,第一个位置右一,第二位置个右二
+					startState = "enterleft",
+					lastTime = 40.0,		                                    --持续时间			
+				},
+			},			
 
 			{
 				time = 6,
@@ -585,10 +617,7 @@ local waves = {
 					id = 1,
 					--demageScale = 2                    --伤害翻1.2倍
 				},
-			},
-
-			-- 			                                                       -- 以上是第四波第一批 11
-			
+			},		                                                       -- 以上是第四波第一批 11
 			{
 				time = 15,	
 				num = 3,
@@ -639,10 +668,7 @@ local waves = {
 					id = 1,
 					--demageScale = 2                    --伤害翻1.2倍
 				},
-			},
-
-			-- 						                                             -- 以上是第四波第二批 21
-
+			},					                                             -- 以上是第四波第二批 21
 			{
 				time = 35,	
 				num = 3,
@@ -705,24 +731,24 @@ local waves = {
 --enemy的关卡配置                                                    黄金镶嵌 m4a1满级  dps大于等于7  怪物数据
 local enemys = {
 	--普通兵                                      140--左右移动距离       280--滚动距离
-	{id=1,image="anim_enemy_002",demage=21,hp=20000,walkRate=180,walkCd=2,rollRate=180,rollCd=2,fireRate=180,fireCd=3,
-	weak1=3},
+	{id=1,image="anim_enemy_002",demage=21,hp=10000,walkRate=180,walkCd=2,rollRate=180,rollCd=2,fireRate=180,fireCd=3,
+	weak1=2},
 
 	--手雷兵      --type = "dao",
-	{id=2,image="shouleib",demage=0,hp=20000,walkRate=180,walkCd=2,rollRate=180,rollCd=2,fireRate=240,fireCd=4,
-	weak1=3},
+	{id=2,image="shouleib",demage=0,hp=10000,walkRate=180,walkCd=2,rollRate=180,rollCd=2,fireRate=240,fireCd=4,
+	weak1=2},
 
 	--手雷            --missileType = "lei",
 	{id=3,image="shoulei",demage=28,hp=211,
-	weak1=3},
+	weak1=1},
 
 	--伞兵       --type = "san",
-	{id=4,image="sanbing01",demage=0,hp=633,
-	weak1=3},	                                                           
+	{id=4,image="sanbing01",demage=0,hp=10000,
+	weak1=2},	                                                           
 
 	--导弹兵      --type = "dao",
 	{id=5,image="zpbing",demage=0,hp=25000,walkRate=120,walkCd=2,fireRate=240,fireCd=5,
-	weak1=3},
+	weak1=2},
 
     --导弹          --missileType = "daodan",
 	{id=6,image="daodan",demage=35,hp=1000,
@@ -730,15 +756,15 @@ local enemys = {
 
 	--近战兵         --type = "jin",          180-- 相对地图的y轴位置       1.7-- 狼牙棒兵 盾兵到身前的比例
 	{id=7,image="jinzhanb",demage=20,hp=30000,fireRate=180,fireCd=4,speed=40,
-	weak1=3},
+	weak1=2},
 
 	--盾兵         --type = "jin",
-	{id=8,image="dunbing",demage=25,hp=100000,fireRate=180,fireCd=5,speed=35,
-	weak1=3},
+	{id=8,image="dunbing",demage=25,hp=90000,fireRate=180,fireCd=5,speed=35,
+	weak1=2},
 
 	--自爆兵        --type = "bao",
 	{id=9,image="zibaob",demage=25,hp=20000,fireRate=30,speed=120,
-	weak1=3},	
+	weak1=2},	
 
 	
 	--人质         type = "renzhi",                                             speakRate =120,speakCd = 5.0,人质喊话cd
@@ -746,28 +772,28 @@ local enemys = {
 	weak1=1},
 
 	--飞机         type = "feiji" ,
-	{id=11,image="feiji",demage=0,hp=200000, walkRate=180,walkCd = 2.0,rollRate=120, rollCd = 1.5, fireRate=120, fireCd=3.0,
-	weak1=3,    award = 60},
+	{id=11,image="feiji",demage=0,hp=120000, walkRate=180,walkCd = 2.0,rollRate=120, rollCd = 1.5, fireRate=120, fireCd=3.0,
+	weak1=2,    award = 60},
 
 	--越野车       type = "jipu" ,
-	{id=12,image="yyc",demage=0,hp=200000,walkRate=180,walkCd = 2.0,rollRate=240, rollCd = 1.5, fireRate=120, fireCd=3.0,
-	weak1=3,    award = 60},
+	{id=12,image="yyc",demage=0,hp=120000,walkRate=180,walkCd = 2.0,rollRate=240, rollCd = 1.5, fireRate=120, fireCd=3.0,
+	weak1=2,    award = 60},
 
 	--金币绿气球   type = "jinbi",
-	{id=13,image="qiqiu03",hp=1,weak1=3,award = 9},	--award = 9   金币数量为9	
+	{id=13,image="qiqiu03",hp=1,weak1=2,award = 9},	--award = 9   金币数量为9	
 
 	--金币蓝气球   type = "jinbi",
-	{id=14,image="qiqiu02",hp=1,weak1=3,award = 15},	--award = 15  金币数量为15
+	{id=14,image="qiqiu02",hp=1,weak1=2,award = 15},	--award = 15  金币数量为15
 
 	--金币黄气球   type = "jinbi",
-	{id=15,image="qiqiu01",hp=1,weak1=3,award = 30},	--award = 30  金币数量为30
+	{id=15,image="qiqiu01",hp=1,weak1=2,award = 30},	--award = 30  金币数量为30
 	--近战boss兵         --type = "jin",
 	{id=16,image="jinzhanb",demage=120,hp=100000,fireRate=60,fireCd=2,speed=40,scale = 2.5 ,
-	weak1=3},                                                          --scale = 3.0,  近战走到屏幕最近放缩比例
+	weak1=2},                                                          --scale = 3.0,  近战走到屏幕最近放缩比例
 
 	--忍者兵            冲锋伤害  type = "renzhe",
 	{id=17,image="renzb",demage=40,hp=50000,walkRate=100,walkCd = 1.0,rollRate=40, rollCd = 1.5,fireRate=180, fireCd=2.0, 
-	shanRate = 100, shanCd = 4, chongRate = 100, chongCd = 4, weak1=3},	
+	shanRate = 100, shanCd = 4, chongRate = 100, chongCd = 4, weak1=2},	
 
 	--飞镖
 	{id=18,image="feibiao",demage=15,hp=8000}, 
@@ -776,7 +802,7 @@ local enemys = {
 	{id=19,image="zzw",demage=10,hp=12500},  
 	--盾兵BOSS         --type = "jin",
 	{id=20,image="dunbing",demage=21,hp=90000,fireRate=60,fireCd= 3 ,speed= 80, scale = 2.6,
-	weak1=3},                           
+	weak1=2},                           
 }
 
 
