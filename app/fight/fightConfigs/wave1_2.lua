f1bee15428d985ae2440b5f2f46b9db96a1723f8
firefly1991 <pengfei99999@qq.com>
2015-02-26 20:53:09 +0800

    no message

--- app/fight/fightConfigs/wave1_2.lua
+++ app/fight/fightConfigs/wave1_2.lua
@@ -3,14 +3,14 @@ local waveClass = class("waveClass", BaseWave)
 
 local waves = {
 	{
-		enemys = {
+		enemys = {                                          --1波1个
 			{
 				time = 1,
 				num = 1,
 				delay = {0},
-				pos = {600},
+				pos = {70},
 				property = { 
-					placeName = "place3",
+					placeName = "place14",
 					startState = "",
 					id = 1,
 				},
@@ -18,14 +18,14 @@ local waves = {
 		},
 	},
 	{
-		enemys = {
+		enemys = {                                          --2波2个
 			{
 				time = 1,
 				num = 2,
 				delay ={0, 0.2},
-				pos = {550,650},		
+				pos = {10,130},		
 				property = { 
-					placeName = "place3",
+					placeName = "place14",
 					startState = "",
 					id = 1,
 				},
@@ -33,14 +33,14 @@ local waves = {
 		},
 	},
 	{
-		enemys = {
+		enemys = {                                          --3波3个 扔雷
 			{
 				time = 1,
 				num = 2,
 				delay ={0, 0.2},
-				pos = {550,650},		
+				pos = {10,130},		
 				property = { 
-					placeName = "place3",
+					placeName = "place14",
 					startState = "",
 					id = 1,
 				},
@@ -49,9 +49,9 @@ local waves = {
 				time = 1,
 				num = 1,
 				delay = {0.1},
-				pos = {600},
+				pos = {70},
 				property = { 
-					placeName = "place3",
+					placeName = "place14",
 					id = 6,
 					startState = "",
 					type = "dao",
@@ -61,9 +61,106 @@ local waves = {
 			},						
 		},
 	},	
-
 	{
-		enemys = { 
+		enemys = {                                           --4波换炮
+			{
+				time = 1,	
+				num = 4,
+				pos = {200,260,330,440,},
+				delay = {0,0.5,0.8,1,},
+				property = { 
+					placeName = "place1",
+					startState = "rollright",
+					id = 1,
+				},
+			},
+			{
+				time = 2,
+				num = 1,
+				pos = {350},
+				delay = {0.1},                            -- 吉普车
+				property = {
+					type = "jipu" ,
+					id = 10,
+					placeName = "place12",
+					missileId = 4,
+					missileType = "daodan",
+					missileOffsets = {cc.p(250,-250), cc.p(-150, -150)},	--炮筒位置发出xy轴偏移值,第一个位置右一,第二位置个右二
+					startState = "enterleft",
+					lastTime = 60.0,		--持续时间			
+				},
+			},	
+			{
+				time = 4,
+				num = 1,
+				delay = {0.5},
+				pos = {800},
+				property = { 
+					placeName = "place1",
+					id = 6,
+					startState = "rollleft",
+					type = "dao",
+					missileId = 7,
+					missileType = "lei",
+				},
+			},
+			{
+				time = 6,
+				num = 1,
+				pos = {800},
+				delay = {0.1},                            -- 吉普车
+				property = {
+					type = "jipu" ,
+					id = 10,
+					placeName = "place12",
+					missileId = 4,
+					missileType = "daodan",
+					missileOffsets = {cc.p(250,-250), cc.p(-150, -150)},	--炮筒位置发出xy轴偏移值,第一个位置右一,第二位置个右二
+					startState = "enterleft",
+					lastTime = 60.0,		--持续时间			
+				},
+			},		
+			{
+				time = 8,
+				num = 1,
+				delay = {0.5},
+				pos = {250},
+				property = { 
+					placeName = "place1",
+					id = 6,
+					startState = "rollright",
+					type = "dao",
+					missileId = 7,
+					missileType = "lei",
+				},
+			},
+			{
+				time = 10,	
+				num = 2,
+				pos = {250,350},
+				delay = {1,1.8},
+				property = { 
+					placeName = "place3",
+					startState = "rollright",
+					id = 1,
+				},
+			},
+
+			{
+				time = 12,	
+				num = 3,
+				pos = {820,750,660},
+				delay = {0,0.6,0.8},
+				property = { 
+					placeName = "place2",
+					startState = "rollleft",
+					id = 1,
+				},
+			},		                                     	
+		},
+	},	
+	{
+		enemys = {                                           --5波换加特林
 			{
 				time = 1,	
 				num = 4,
@@ -91,7 +188,7 @@ local waves = {
 				},
 			},
 			{
-				time = 2,
+				time = 3,
 				num = 1,
 				delay = {0.5},
 				pos = {250},
@@ -105,7 +202,7 @@ local waves = {
 				},
 			},
 			{
-				time = 1,	
+				time = 4,	
 				num = 2,
 				pos = {250,350},
 				delay = {1,1.8},
@@ -117,7 +214,7 @@ local waves = {
 			},
 
 			{
-				time = 3,	
+				time = 5,	
 				num = 3,
 				pos = {820,750,660},
 				delay = {0,0.6,0.8},
@@ -127,11 +224,8 @@ local waves = {
 					id = 1,
 				},
 			},
-
-						-- 以上是第三波第一批
-			
 			{
-				time = 5,	
+				time = 6,	
 				num = 3,
 				pos = {240,370,300},
 				delay = {1,1.5,1.8},
@@ -153,7 +247,7 @@ local waves = {
 				},
 			},
 			{
-				time = 7,
+				time = 10,
 				num = 2,
 				delay = {0.2,0},
 				pos = {250,900},
@@ -167,7 +261,7 @@ local waves = {
 				},
 			},	
 			{
-				time = 9,	
+				time = 12,	
 				num = 2,
 				pos = {870,950},
 				delay = {0,0.6},
@@ -177,63 +271,55 @@ local waves = {
 					id = 1,
 				},
 			},
-
-									-- 以上是第一波第二批
-
 			{
-				time = 5,	
+				time = 14,	
 				num = 3,
 				pos = {240,370,300},
 				delay = {1,1.5,1.8},
 				property = { 
 					placeName = "place1",
-					startState = "rollright",
-					id = 1,
+					id = 2,
+					type = "jin",
 				},
 			},		
 			{
-				time = 8,	
+				time = 16,	
 				num = 2,
 				pos = {1000,750},
 				delay = {0,0.8},
 				property = {
-					placeName = "place4", 
-					startState = "rollleft",
-					id = 1,
+					placeName = "place2",
+					id = 2,
+					type = "jin",
 				},
 			},
 			{
-				time = 7,
+				time = 18,
 				num = 2,
 				delay = {0.2,0},
-				pos = {250,900},
+				pos = {250,700},
 				property = { 
-					placeName = "place3",
-					id = 6,
-					startState = "rollright",
-					type = "dao",
-					missileId = 7,
-					missileType = "lei",
+					placeName = "place1",
+					id = 2,
+					type = "jin",
 				},
 			},	
 			{
-				time = 9,	
+				time = 20,	
 				num = 3,
 				pos = {820,910,980},
 				delay = {0,0.6,0.8},
 				property = {
-					placeName = "place2", 
-					startState = "rollleft",
-					id = 1,
+					placeName = "place2",
+					id = 2,
+					type = "jin",
 				},
 			},
-		                                     	-- 以上是第一波第三批31
+		                                     	
 		},
 	},	
 	{
-		enemys = {                                           
-		                                                                -- 第二波
-
+		enemys = {                                           -- 第6波空军+飞机
 			{
 				time = 1,
 				num = 3,
@@ -246,7 +332,7 @@ local waves = {
 				},
 			},
 			{
-				time = 1.5,
+				time = 2,
 				num = 3,
 				delay = {0, 0.3, 0.7},
 				pos = {800,650,900},	
@@ -255,21 +341,9 @@ local waves = {
 					startState = "rollleft",
 					id = 1,
 				},
-			},	
+			},
 			{
-				time = 3,
-				num = 3,
-				delay = {0.2,0.3, 0.6},
-				pos = {470,550,700},					
-				property = {
-					placeName = "place2",  
-					type = "san",
-					id = 3,
-					enemyId = 1,
-				},
-			},	
-			{
-				time = 1.2,	
+				time = 2,	
 				num = 1,
 				pos = {300},
 				delay = {0.5},
@@ -282,8 +356,34 @@ local waves = {
 					missileType = "lei",
 				},
 			},
-
-			                                          -- 以上是第二波第一批10
+			{
+				time = 3,
+				num = 1,
+				pos = {450},
+				delay = {0.5},                         -- 飞机
+				property = {
+					type = "feiji" ,
+					id = 9,
+					placeName = "place11",
+					missileId = 4,
+					missileType = "daodan",
+					missileOffsets = {cc.p(250,-250), cc.p(-150, -150)},	--炮筒位置发出xy轴偏移值,第一个位置右一,第二位置个右二
+					startState = "enterleft",
+					lastTime = 60.0,		                                    --持续时间			
+				},
+			},		
+			{
+				time = 4,
+				num = 3,
+				delay = {0.2,0.3, 0.6},
+				pos = {470,550,700},					
+				property = {
+					placeName = "place2",  
+					type = "san",
+					id = 3,
+					enemyId = 1,
+				},
+			},
 			{
 				time = 6,	
 				num = 8,
@@ -297,6 +397,22 @@ local waves = {
 				},
 			},	
 			{
+				time = 8,
+				num = 1,
+				pos = {450},
+				delay = {0.5},                         -- 飞机
+				property = {
+					type = "feiji" ,
+					id = 9,
+					placeName = "place11",
+					missileId = 4,
+					missileType = "daodan",
+					missileOffsets = {cc.p(250,-250), cc.p(-150, -150)},	--炮筒位置发出xy轴偏移值,第一个位置右一,第二位置个右二
+					startState = "enterleft",
+					lastTime = 60.0,		                                    --持续时间			
+				},
+			},		
+			{
 				time = 9,	
 				num = 2,
 				pos = {350,700},
@@ -308,8 +424,6 @@ local waves = {
 					placeName = "place3",
 				},
 			},
-			
-                                              -- 以上是第二波第二批10
 			{
 				time = 10,
 				num = 3,
@@ -358,8 +472,6 @@ local waves = {
 					missileType = "lei",
 				},
 			},
- 			                                       -- 以上是第二波第三批 10    
-
 		},
 
 	},
@@ -388,19 +500,19 @@ local waves = {
 --enemy的关卡配置                           无镶嵌 MP5伤害65  dps大于等于1 怪物数据
 local enemys = {
 	--普通兵
-	{id=1,image="anim_enemy_002",demage=1,hp=160,walkRate=180,walkCd=2,rollRate=1800,rollCd=120,fireRate=180,fireCd=3,
-	weak1=2},
+	{id=1,image="anim_enemy_002",demage=1,hp=160,walkRate=180,walkCd=2,rollRate=180,rollCd=2,fireRate=180,fireCd=3,
+	weak1=3},
 
 	--近战兵
-	{id=2,image="jinzhanb",demage=15,hp=190,walkRate=180,walkCd=2,rollRate=120,rollCd=2,fireRate=180,fireCd=3,
-	weak1=2},
+	{id=2,image="jinzhanb",demage=3,hp=190,fireRate=180,fireCd=3,speed=40,scale = 1.5,
+	weak1=3},                                                           --scale = 3.0,  近战走到屏幕最近放缩比例
 
 	--伞兵
 	{id=3,image="sanbing01",demage=0,hp=160,
 	weak1=2},	             
 
     --导弹          --missileType = "daodan",
-	{id=4,image="daodan",demage=5,hp=1,
+	{id=4,image="daodan",demage=1,hp=1,
 	weak1=1},
 
 	--铁球
@@ -408,7 +520,7 @@ local enemys = {
 	{id=5,image="tieqiu",demage=20,hp=3760,weak1=1},	
 
 	--手雷兵
-	{id=6,image="shouleib",demage=0,hp=120,walkRate=180,walkCd=2,rollRate=1800,rollCd=120,fireRate=180,fireCd=4,
+	{id=6,image="shouleib",demage=0,hp=120,walkRate=180,walkCd=2,rollRate=180,rollCd=2,fireRate=180,fireCd=4,
 	weak1=2},
 	--手雷
 	{id=7,image="shoulei",demage=1,hp=1,
@@ -417,6 +529,13 @@ local enemys = {
 	{id=8,image="daodan",demage=5,hp=120,
 	weak1=1},
 
+	--飞机         type = "feiji" ,
+	{id=9,image="feiji",demage=0,hp=1600, walkRate=180,walkCd = 2.0,rollRate=120, rollCd = 1.5, fireRate=180, fireCd=4.0,
+	weak1=2,    award = 60},
+
+	--越野车       type = "jipu" ,
+	{id=10,image="yyc",demage=0,hp=1600,walkRate=180,walkCd = 2.0,rollRate=240, rollCd = 1.5, fireRate=240, fireCd=5.0,
+	weak1=2,    award = 60},
 
 }
 
@@ -501,7 +620,7 @@ function waveClass:ctor()
 	self.bosses = bosses
 	self.mapId  = mapId
 	self.limit  = limit
-	self.goldLimits = {25,55,95}   --黄武激活所需杀人个数  本关共60个小怪
+	self.goldLimits = {225,355,495}   --黄武激活所需杀人个数  本关共60个小怪
 end
 
 return waveClass
