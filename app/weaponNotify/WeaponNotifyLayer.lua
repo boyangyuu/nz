51050f2b11b14cd019c9c1ea426f6335ec429bb5
DonnieSuen <651991167@qq.com>
2015-03-24 20:12:30 +0800

    no message

---
+++ app/weaponNotify/WeaponNotifyLayer.lua
@@ -0,0 +1,13 @@
+local WeaponNotifyLayer = class("WeaponNotifyLayer", function()
+	return display.newLayer()
+end)
+
+function WeaponNotifyLayer:ctor(properties)
+
+end
+
+function WeaponNotifyLayer:loadCCS()
+	
+end
+
+return WeaponNotifyLayer
