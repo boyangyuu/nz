c2d5708e786927fa84619fcb13c65468ae9b3932
firefly1991 <pengfei99999@qq.com>
2015-04-09 16:05:04 +0800

    no message

--- app/fight/fightConfigs/wave3_1.lua
+++ app/fight/fightConfigs/wave3_1.lua
@@ -164,62 +164,9 @@ local waves = {
 
 --enemy的关卡配置                                                     黄金镶嵌M4A1满级1枪伤害621  dps大于等于6  怪物数据
 local enemys = {
-	--普通兵                                      140--左右移动距离       280--滚动距离
-	{id=1,image="anim_enemy_002",demage=21,hp=10000,walkRate=180,walkCd=2,rollRate=180,rollCd=2,fireRate=180,fireCd=4,
-	weak1=2},
-
-	--手雷兵      --type = "dao",
-	{id=2,image="shouleib",demage=0,hp=10000,walkRate=180,walkCd=2,rollRate=180,rollCd=2,fireRate=240,fireCd=4,
-	weak1=2},
-
-	--手雷            --missileType = "lei",
-	{id=3,image="shoulei",demage=28,hp=1000,
-	weak1=1},
-                                                          
-	--导弹兵      --type = "dao",
-	{id=5,image="zpbing",demage=0,hp=25000,walkRate=120,walkCd=2,fireRate=240,fireCd=5,
-	weak1=2},
-
-    --导弹          --missileType = "daodan",
-	{id=6,image="daodan",demage=35,hp=3000,
-	weak1=1},	
-
-	--近战兵         --type = "jin",          180-- 相对地图的y轴位置       1.7-- 狼牙棒兵 盾兵到身前的比例
-	{id=7,image="jinzhanb",demage=20,hp=25000,fireRate=180,fireCd=4,speed=40,
-	weak1=2},
 
 	--盾兵         --type = "jin",
-	{id=8,image="dunbing",demage=25,hp=70000,fireRate=180,fireCd=5,speed=35,
-	weak1=2},
-
-	--自爆兵        --type = "bao",
-	{id=9,image="zibaob",demage=25,hp=5000,fireRate=30,speed=120,
-	weak1=2},	
-
-	
-	--人质         type = "renzhi",                                             speakRate =120,speakCd = 5.0,人质喊话cd
-	{id=10,image="hs",demage=0,hp=1,walkRate=120,walkCd = 1.0,rollRate=180,rollCd=2, speakRate =240,speakCd = 5.0,
-	weak1=1},
-
-	--飞机         type = "feiji" ,
-	{id=11,image="feiji",demage=0,hp=70000, walkRate=180,walkCd = 2.0,rollRate=120, rollCd = 1.5, fireRate=120, fireCd=3.0,
-	weak1=2,    award = 60},
-
-	--越野车       type = "jipu" ,
-	{id=12,image="yyc",demage=0,hp=70000,walkRate=180,walkCd = 2.0,rollRate=240, rollCd = 1.5, fireRate=120, fireCd=3.0,
-	weak1=2,    award = 60},
-
-	--金币绿气球   type = "jinbi",
-	{id=13,image="qiqiu03",hp=1,weak1=2,award = 9},	--award = 9   金币数量为9	
-
-	--金币蓝气球   type = "jinbi",
-	{id=14,image="qiqiu02",hp=1,weak1=2,award = 15},	--award = 15  金币数量为15
-
-	--金币黄气球   type = "jinbi",
-	{id=15,image="qiqiu01",hp=1,weak1=2,award = 30},	--award = 30  金币数量为30
-	--近战boss兵         --type = "jin",
-	{id=16,image="jinzhanb",demage=120,hp=100000,fireRate=60,fireCd=2,speed=40,scale = 2.5 ,
-	weak1=2},                                                          --scale = 3.0,  近战走到屏幕最近放缩比例
+	{id=8,image="dunbing",demage=25,hp=50000,fireRate=180,fireCd=5,speed=35, weak1=2},
 
 	--忍者兵            冲锋伤害  type = "renzhe",
 	{id=17,image="xiaorz",demage=40,hp=35000,walkRate=100,walkCd = 1.0,rollRate=40, rollCd = 1.5,fireRate=180, fireCd=2.0, 
--- app/fight/fightConfigs/wave3_4.lua
+++ app/fight/fightConfigs/wave3_4.lua
@@ -6,97 +6,75 @@ local waves = {
 		enemys = { 
 			{
 				time = 2,
-				num = 2,
-				delay = {0.3,1.0},
-				pos = {300,550},
-				property = {
-					placeName = "place9",  
-					startState = "rollright",
-					id = 1,
-				},
-			},
-
-			{
-				time = 5,
 				num = 1,
 				delay = {0.5},
-				pos = {360},
+				pos = {0},
 				property = {
-					placeName = "place5",  
-					startState = "rollleft",
-					id = 1,
+					type = "taofan_qiu", 
+					placeName = "place5",
+					id = 4,
+					startState = "enterleft", --从屏幕左侧进入
+					data = {
+						direct = "right", --向右逃跑
+						{
+							pos = 160,  --第一次藏身处 移动 600
+							time = 4,   --隐藏时间 3s	
+						},	
+						{
+							pos = 500,  --第一次藏身处 移动 600
+							time = 4,   --隐藏时间 3s
+						},										
+					},									
 				},
 			},
-			{
-				time = 8,
-				num = 1,
-				delay = {0},
-				pos = {500},
-				property = {
-					placeName = "place11",  
-					startState = "rollleft",
-					id = 2,
-					type = "dao",
-					missileId = 3,
-					missileType = "lei",
-				},
-			},
-			{
-				
-				time = 11,
-				num = 1,
-				delay = {0},
-				pos = {50},
-				property = {
-					placeName = "place10",  
-					id = 2,
-					type = "dao",
-					missileId = 3,
-					missileType = "lei",
-				},
-			},
-			{
-				time = 14,
-				num = 1,
-				delay = {0},
-				pos = {500},
-				property = {
-					placeName = "place9",  
-					startState = "rollright",
-					id = 2,
-					type = "dao",
-					missileId = 3,
-					missileType = "lei",
-				},
-			},
-			{
-				
-				time = 17,
-				num = 2,
-				delay = {0.3,1.6},
-				pos = {90,280},
-				property = {
-					placeName = "place16",
-					id = 5,
-					type = "dao",
-					missileId = 6,
-					missileType = "daodan",
-				},
-			},
-			{
-				time = 21,
-				num = 2,
-				delay = {0.6,1.1},
-				pos = {300,500},
-				property = {
-					placeName = "place9",  
-					startState = "rollright",
-					id = 2,
-					type = "dao",
-					missileId = 3,
-					missileType = "lei",                                         --10
-				},
-			},	
+			-- {
+			-- 	time = 6,
+			-- 	num = 1,
+			-- 	delay = {0.5},
+			-- 	pos = {0},
+			-- 	property = {
+			-- 		type = "taofan_qiu", 
+			-- 		placeName = "place9",
+			-- 		id = 4,
+			-- 		startState = "enterleft", --从屏幕左侧进入
+			-- 		data = {
+			-- 			direct = "right", --向右逃跑
+			-- 			{
+			-- 				pos = 300,  --第一次藏身处 移动 600
+			-- 				time = 3,   --隐藏时间 3s	
+			-- 			},
+			-- 			{
+			-- 				pos = 300,  --第一次藏身处 移动 600
+			-- 				time = 3,   --隐藏时间 3s	
+			-- 			},
+			-- 		},
+			-- 	},
+			-- },
+			-- {
+			-- 	time = 10,
+			-- 	num = 1,
+			-- 	delay = {0.5},
+			-- 	pos = {0},
+			-- 	property = {
+			-- 		type = "taofan_qiu", 
+			-- 		placeName = "place11",
+			-- 		id = 4,
+			-- 		startState = "enterright", --从屏幕左侧进入
+			-- 		data = {
+			-- 			direct = "left", --向右逃跑
+			-- 			{
+			-- 				pos = 650,  --第一次藏身处 移动 600
+			-- 				time = 3,   --隐藏时间 3s	
+			-- 			},
+			-- 			{
+			-- 				pos = 300,  --第一次藏身处 移动 600
+			-- 				time = 3,   --隐藏时间 3s	
+			-- 			},
+			-- 		},
+			-- 	},
+			-- },
+			
+
 		},
 	},
 
@@ -283,33 +261,7 @@ local waves = {
 					startState = "san",
 					id = 1,
 				},
-			},
-			{
-				time = 3,	      --忍者
-				num = 1,
-				pos = {300},
-				delay = {0.2},
-				property = {
-					placeName = "place9" ,
-					id = 17,
-					type = "renzhe",
-					missileId = 18,
-				},
-			},	
-			{
-				time = 37,	      --忍者
-				num = 2,
-				pos = {400,700},
-				delay = {0.5,1.3},
-				property = {
-					placeName = "place11" ,
-					id = 17,
-					type = "renzhe",
-					missileId = 18,
-				},
-			},			
-			
-			
+			},					
 		},
 	},
 
@@ -330,6 +282,9 @@ local enemys = {
 	--手雷            --missileType = "lei",
 	{id=3,image="shoulei",demage=20,hp=420,
 	weak1=1},
+
+	--囚犯            type = "taofan_qiu",
+	{id=4,image="qiufan",demage=5,hp=2500, weak1=2,},
                                                         
 	--导弹兵      --type = "dao",
 	{id=5,image="zpbing",demage=0,hp=5000,walkRate=120,walkCd=2,fireRate=240,fireCd=5,
@@ -339,52 +294,11 @@ local enemys = {
 	{id=6,image="daodan",demage=25,hp=420,
 	weak1=1},	
 
-	--近战兵         --type = "jin",          180-- 相对地图的y轴位置       1.7-- 狼牙棒兵 盾兵到身前的比例
-	{id=7,image="jinzhanb",demage=20,hp=936,fireRate=180,fireCd=4,speed=40,
-	weak1=2},
-
-	--盾兵         --type = "jin",
-	{id=8,image="dunbing",demage=25,hp=3744,fireRate=180,fireCd=5,speed=35,
-	weak1=2},
-
-	--自爆兵        --type = "bao",
-	{id=9,image="zibaob",demage=60,hp=562,fireRate=30,speed=100,
-	weak1=2},	
-
-	
-	--人质         type = "renzhi",                                             speakRate =120,speakCd = 5.0,人质喊话cd
-	{id=10,image="hs",demage=0,hp=6666,walkRate=120,walkCd = 1.0,rollRate=180,rollCd=2, speakRate =240,speakCd = 5.0,
-	weak1=2},
-
 	--飞机         type = "feiji" ,
 	{id=11,image="feiji",demage=0,hp=10870, walkRate=180,walkCd = 2.0,rollRate=120, rollCd = 1.5, fireRate=180, fireCd=4.0,
 	weak1=2.0,    award = 60},
 
-	--越野车       type = "jipu" ,
-	{id=12,image="yyc",demage=0,hp=10000,walkRate=180,walkCd = 2.0,rollRate=240, rollCd = 1.5, fireRate=120, fireCd=3.0,
-	weak1=2,    award = 60},
 
-	--金币绿气球   type = "jinbi",
-	{id=13,image="qiqiu03",hp=1,weak1=2,award = 9},	--award = 9   金币数量为9	
-
-	--金币蓝气球   type = "jinbi",
-	{id=14,image="qiqiu02",hp=1,weak1=2,award = 15},	--award = 15  金币数量为15
-
-	--金币黄气球   type = "jinbi",
-	{id=15,image="qiqiu01",hp=1,weak1=2,award = 30},	--award = 30  金币数量为30
-	--近战boss兵         --type = "jin",
-	{id=16,image="jinzhanb",demage=120,hp=20000,fireRate=60,fireCd=2,speed=40,scale = 2.5 ,
-	weak1=2},                                                          --scale = 3.0,  近战走到屏幕最近放缩比例
-
-	--忍者兵            冲锋伤害  type = "renzhe",
-	{id=17,image="xiaorz",demage=40,hp=6000,walkRate=100,walkCd = 1.0,rollRate=40, rollCd = 1.5,fireRate=180, fireCd=2.0, 
-	shanRate = 100, shanCd = 4, chongRate = 100, chongCd = 4, weak1=2},	
-
-	--飞镖
-	{id=18,image="feibiao",demage=15,hp=800}, 
-	
-	--蜘蛛网
-	{id=19,image="zzw",demage=10,hp=12500},                             
 }
 
 
@@ -401,7 +315,7 @@ function waveClass:ctor()
 	self.bosses = bosses
 	self.mapId  = mapId
 	self.fightMode =  {
-		type 	  = "puTong",
+		-- type 	  = "puTong",
 
 		-- type 	  = "renZhi",
 		-- saveNums  = 4,                 --解救人质数量
@@ -409,8 +323,8 @@ function waveClass:ctor()
 		-- type 	  = "xianShi",
 		-- limitTime = 60,                   --限时模式时长
 
-		-- type 	  = "taoFan"
-		-- limitNums = 5,                      --逃跑逃犯数量
+		type 	  = "taoFan",
+		limitNums = 5,                      --逃跑逃犯数量
 	}
 end
 return waveClass
