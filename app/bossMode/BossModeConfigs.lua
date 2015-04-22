6f21616656baae175c4c32fd0875d183bbc15a68
DonnieSuen <651991167@qq.com>
2015-04-22 11:38:15 +0800

    no message

--- app/bossMode/BossModeConfigs.lua
+++ app/bossMode/BossModeConfigs.lua
@@ -6,27 +6,24 @@ BossConfigs["chapter1"] = {
 	name = "丛林野战",
 	desc = "决战吧！@小骚辉",
 	weaponid = 2,
-	weaponName = "怒火麒麟",
 	weaponSkill = "多重攻击",
-	weaponSkill = "weaponSkill!!!adifgaiousgfagiuawrfvbasg",
+	weaponSkillDesc = "weaponSkill!!!adifgaiousgfagiuawrfvbasg",
 }
 
 BossConfigs["chapter2"] = {
 	name = "野战",
 	desc = "决战吧！@大骚辉",
 	weaponid = 3,
-	weaponName = "怒火麒麟",
 	weaponSkill = "多重攻击",
-	weaponSkill = "ll!!!adifgaiousgfagiuawrfvbasg",
+	weaponSkillDesc = "ll!!!adifgaiousgfagiuawrfvbasg",
 }
 
 BossConfigs["chapter3"] = {
 	name = "丛林",
 	desc = "决战吧！@老骚辉",
 	weaponid = 6,
-	weaponName = "怒火麒麟",
 	weaponSkill = "多重攻击",
-	weaponSkill = "weSkill!!!adifgaiousgfagiuawrfvbasg",
+	weaponSkillDesc = "weSkill!!!adifgaiousgfagiuawrfvbasg",
 }
  
 function BossModeConfigs.getConfig(chapterId)
