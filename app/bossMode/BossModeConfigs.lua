c04842b252a59eb4d9987aa6bb110ce5e8455a20
firefly1991 <pengfei99999@qq.com>
2015-05-19 21:54:01 +0800

    ls

--- app/bossMode/BossModeConfigs.lua
+++ app/bossMode/BossModeConfigs.lua
@@ -7,11 +7,11 @@ BossConfigs["chapter1"] = {
 	desc = "挑战胜利后获得极品武器、医疗包、手雷及大量金币奖励！",
 	weaponId = 9,
 	weaponSkill = "麒麟之怒",
-	reward1 = {{part = 1},{lei = 5},{money = 10000}},
-	reward2 = {{part = 1},{healthBag = 1},{lei = 5},{money = 10000}},
-	reward3 = {{part = 1},{healthBag = 2},{lei = 10},{money = 20000}},
-	reward4 = {{part = 1},{healthBag = 3},{lei = 10},{money = 30000}},
-	reward5 = {{part = 1},{healthBag = 4},{lei = 20},{money = 50000}},
+	reward1 = {{part = 1},{healthBag = 1},{lei = 5},{money = 20000}},
+	reward2 = {{part = 1},{healthBag = 2},{lei = 5},{money = 20000}},
+	reward3 = {{part = 1},{healthBag = 3},{lei = 10},{money = 30000}},
+	reward4 = {{part = 1},{healthBag = 4},{lei = 10},{money = 30000}},
+	reward5 = {{part = 1},{healthBag = 5},{lei = 20},{money = 50000}},
 }
 
 
